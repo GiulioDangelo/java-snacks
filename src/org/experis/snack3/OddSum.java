8f1f34ea47cdc879a3757e3f00681368e0e5d2d6
GiulioDangelo <dangelogiulio48@gmail.com>
2024-03-28 15:06:44 +0100

    add 3 snack

--- src/org/experis/snack3/OddSum.java
+++ src/org/experis/snack3/OddSum.java
@@ -1,2 +1,16 @@
-package org.experis.snack3;public class OddSum {
+package org.experis.snack3;
+
+public class OddSum {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] % 2 == 1) {
+                sum += num[i];
+            }
+        }
+
+        System.out.println(sum);
+    }
 }
