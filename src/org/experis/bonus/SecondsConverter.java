2531418af1aabcea06fa6aabd6cf826e82907cf8
GiulioDangelo <dangelogiulio48@gmail.com>
2024-03-28 16:37:54 +0100

    push

--- src/org/experis/bonus/SecondsConverter.java
+++ src/org/experis/bonus/SecondsConverter.java
@@ -2,6 +2,15 @@ package org.experis.bonus;
 
 public class SecondsConverter {
     public static void main(String[] args) {
+        int value = 3661;
 
+        int hours = value / 3600;
+        int minutes = (value % 3600) / 60;
+        int seconds = value % 60;
+
+        String timeFormatted = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+
+        System.out.println(seconds + " secondi equivalgono a: " + timeFormatted);
     }
+
 }
